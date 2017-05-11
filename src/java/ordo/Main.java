0b21482e7921f813261fb6fda74324a1b8ba7e68
Nicolas Pendon <nicolaspendon@hotmail.fr>
2017-05-11 10:38:49 +0200

    Ajout d'un MAIN pour les premières étapes du sujet

---
+++ src/java/ordo/Main.java
@@ -0,0 +1,11 @@
+package ordo;
+
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("WORKS");
+    }
+}
