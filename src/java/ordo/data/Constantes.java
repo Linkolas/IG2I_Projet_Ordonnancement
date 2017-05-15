94df5f2178e0a00dffe191332845f5a53772cfb3
Nicolas Pendon <nicolaspendon@hotmail.fr>
2017-05-15 14:22:40 +0200

    + Classe Constantes

---
+++ src/java/ordo/data/Constantes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordo.data;
+
+/**
+ *
+ * @author Nicolas
+ */
+public abstract class Constantes {
+    
+    public static float capaciteMax = -1;
+    public static float dureeMaxTournee = -1;
+    
+    public static float dureePark = -1;
+    public static float dureePickup = -1;
+    public static float dureeSwap = -1;
+    public static float dureeExchange = -1;
+    
+    
+    public static float coutTracteur = -1;
+    public static float coutTrain = -1;
+    public static float coutDureeTracteur = -1;
+    public static float coutTrajetTracteur = -1;
+    public static float coutSecondeRemorque = -1;
+    public static float coutTrajetSecondeRemorque = -1;
+    
+}
