e065bd5f196e67c5d3f5c46d2880fbc9bb12220e
Nicolas Pendon <nicolaspendon@hotmail.fr>
2017-05-15 12:41:19 +0200

    ~ Suppression champs doublons

--- src/java/ordo/data/entities/Depot.java
+++ src/java/ordo/data/entities/Depot.java
@@ -18,48 +18,13 @@ import javax.persistence.Id;
 @Entity
 public class Depot extends Lieu implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
     public Depot() {
     }
     
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    
     // <editor-fold defaultstate="collapsed" desc=".equals, .toString, ...">
-    private static final long serialVersionUID = 1L;
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Depot)) {
-            return false;
-        }
-        Depot other = (Depot) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
     @Override
     public String toString() {
-        return "ordo.data.entities.Depot[ id=" + id + " ]";
+        return "ordo.data.entities.Depot[ id=" + getId() + " ]";
     }
     //</editor-fold>
 }
--- src/java/ordo/data/entities/SwapLocation.java
+++ src/java/ordo/data/entities/SwapLocation.java
@@ -19,22 +19,10 @@ import javax.persistence.Id;
 @Entity
 public class SwapLocation extends Lieu implements Serializable {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    
     @Column
     private String libelle;
 
     public SwapLocation() {
-    }
-    
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
     
     public String getLibelle() {
@@ -47,31 +35,9 @@ public class SwapLocation extends Lieu implements Serializable {
 
     
     // <editor-fold defaultstate="collapsed" desc=".equals, .toString, ...">
-    private static final long serialVersionUID = 1L;
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof SwapLocation)) {
-            return false;
-        }
-        SwapLocation other = (SwapLocation) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
     @Override
     public String toString() {
-        return "ordo.data.entities.Depot[ id=" + id + " ]";
+        return "ordo.data.entities.Depot[ id=" + getId() + " ]";
     }
     //</editor-fold>
 }
