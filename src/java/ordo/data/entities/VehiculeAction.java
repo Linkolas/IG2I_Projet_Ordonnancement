13406974df1ca03d90775f3194871b0fcc11fcb8
Florent BRASSART <florent.brassart@ig2i.fr>
2017-06-10 17:25:55 +0200

    Création de la namedquery VehiculeAction.FindByVehicule

--- src/java/ordo/data/entities/VehiculeAction.java
+++ src/java/ordo/data/entities/VehiculeAction.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 /**
@@ -21,6 +22,7 @@ import javax.persistence.OneToMany;
  * @author Nicolas
  */
 @Entity
+@NamedQuery(name="VehiculeAction.findByVehicule", query="SELECT va FROM VehiculeAction WHERE va.vehicule_id = :vehiculeId ") 
 public class VehiculeAction implements Serializable {
 
     @Id
