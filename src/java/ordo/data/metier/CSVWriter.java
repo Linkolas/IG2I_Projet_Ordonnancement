80971182877cb401d4a5630493ba66927894f060
Florent BRASSART <florent.brassart@ig2i.fr>
2017-06-10 17:27:10 +0200

    Première version de CSVWriter, prend en compte uniquement la première ligne d'une tournée

--- src/java/ordo/data/metier/CSVWriter.java
+++ src/java/ordo/data/metier/CSVWriter.java
@@ -7,8 +7,14 @@ package ordo.data.metier;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import ordo.data.dao.jpa.JpaSolutionDao;
+import ordo.data.dao.jpa.JpaVehiculeActionDao;
+import ordo.data.dao.jpa.JpaVehiculeDao;
+import ordo.data.entities.Vehicule;
+import ordo.data.entities.VehiculeAction;
 
 /**
  *
@@ -16,6 +22,9 @@ import java.util.logging.Logger;
  */
 public class CSVWriter
 {
+    private static final String DELIMITEUR = ";";
+    private static final String SEPARATEUR_LIGNE = "\n";
+    
     public CSVWriter()
     {
     }
@@ -24,6 +33,9 @@ public class CSVWriter
     {
         try
         {
+            JpaVehiculeDao jpaVehiculeDao = JpaVehiculeDao.getInstance();
+            JpaVehiculeActionDao jpaVehiculeActionDao = JpaVehiculeActionDao.getInstance();
+            
             //On génère le fichier nommé Solution.csv
             FileWriter filewriter = null;           
             filewriter = new FileWriter("Solution.csv");
@@ -31,9 +43,84 @@ public class CSVWriter
             //On inscrit le header qui respecte le format demandé
             filewriter.append("TOUR_ID;TOUR_POSITION;LOCATION_ID;LOCATION_TYPE;SEMI_TRAILER_ATTACHED;SWAP_BODY_TRUCK;SWAP_BODY_SEMI_TRAILER;SWAP_ACTION;SWAP_BODY_1_QUANTITY;SWAP_BODY_2_QUANTITY");
             //Puis on passe à la ligne
-            filewriter.append("\n");
+            filewriter.append(SEPARATEUR_LIGNE);
+            
+                
+            List<Vehicule> vehicules = (List<Vehicule>) jpaVehiculeDao.findAll();
+            List<VehiculeAction> vehiculeActions;
+            String TOUR_ID, 
+                    TOUR_POSITION, 
+                    LOCATION_ID, 
+                    LOCATION_TYPE, 
+                    SEMI_TRAILER_ATTACHED, 
+                    SWAP_BODY_TRUCK, 
+                    SWAP_BODY_SEMI_TRAILLER,
+                    SWAP_ACTION,
+                    SWAP_BODY_1_QUANTITY,
+                    SWAP_BODY_2_QUANTITY;
+            
+            for(Vehicule vehicule: vehicules)
+            {
+                Integer i=1;
+                TOUR_ID = 'R'+ i.toString();
+                
+                vehiculeActions = jpaVehiculeActionDao.findByVehicule(vehicule);
+                
+                for(VehiculeAction vehiculeAction: vehiculeActions)
+                {
+                    Integer j=1;
+                    TOUR_POSITION = j.toString();
+                    
+                    if(j == 1)
+                    {
+                        LOCATION_ID = "D1";
+                        LOCATION_TYPE = "DEPOT";
+                        if(vehicule.isTrain())
+                        {
+                            SEMI_TRAILER_ATTACHED = "1";
+                            SWAP_BODY_TRUCK = "1";
+                            SWAP_BODY_SEMI_TRAILLER = "2";
+                        }
+                        else
+                        {
+                            SEMI_TRAILER_ATTACHED = "0";
+                            SWAP_BODY_TRUCK = "1";
+                            SWAP_BODY_SEMI_TRAILLER = "0";
+                        }
+                        SWAP_ACTION = "NONE";
+                        SWAP_BODY_1_QUANTITY = "0";
+                        SWAP_BODY_2_QUANTITY = "0";
+                    }
+                    else
+                    {
+                        LOCATION_ID = "";
+                        LOCATION_TYPE ="";
+                        SEMI_TRAILER_ATTACHED = "";
+                        SWAP_BODY_TRUCK = "";
+                        SWAP_BODY_SEMI_TRAILLER = "";
+                        SWAP_ACTION = "";
+                        SWAP_BODY_1_QUANTITY = "";
+                        SWAP_BODY_2_QUANTITY = "";
+                    }
+                    filewriter.append(TOUR_ID);                 filewriter.append(DELIMITEUR);
+                    filewriter.append(TOUR_POSITION);           filewriter.append(DELIMITEUR);
+                    filewriter.append(LOCATION_ID);             filewriter.append(DELIMITEUR);
+                    filewriter.append(LOCATION_TYPE);           filewriter.append(DELIMITEUR);
+                    filewriter.append(SEMI_TRAILER_ATTACHED);   filewriter.append(DELIMITEUR);
+                    filewriter.append(SWAP_BODY_TRUCK);         filewriter.append(DELIMITEUR);
+                    filewriter.append(SWAP_BODY_SEMI_TRAILLER); filewriter.append(DELIMITEUR);
+                    filewriter.append(SWAP_ACTION);             filewriter.append(DELIMITEUR);
+                    filewriter.append(SWAP_BODY_1_QUANTITY);    filewriter.append(DELIMITEUR);
+                    filewriter.append(SWAP_BODY_2_QUANTITY);    filewriter.append(DELIMITEUR);
+                    
+                    filewriter.append(SEPARATEUR_LIGNE);
+                    j++;
+                }
+                i++;
+            }
             
             //TODO : récupérer la solution et ses informations liées 
+            
             
             //On flush puis on ferme 
             filewriter.flush();
