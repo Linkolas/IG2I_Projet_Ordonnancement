ca83be284b07df04c9a2890bfcd73476ce5b4718
AxelleG <axelle.galant@ig2i.fr>
2017-06-14 16:14:58 +0200

    Début mise en place algo random

---
+++ src/java/ordo/algo/AlgoRandom.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordo.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+import ordo.data.dao.jpa.JpaCommandeClientDao;
+import ordo.data.dao.jpa.JpaDepotDao;
+import ordo.data.dao.jpa.JpaLieuDao;
+import ordo.data.dao.jpa.JpaSolutionDao;
+import ordo.data.dao.jpa.JpaSwapBodyDao;
+import ordo.data.dao.jpa.JpaTrajetDao;
+import ordo.data.dao.jpa.JpaVehiculeActionDao;
+import ordo.data.dao.jpa.JpaVehiculeDao;
+import ordo.data.entities.CommandeClient;
+import ordo.data.entities.Depot;
+import ordo.data.entities.Lieu;
+import ordo.data.entities.Trajet;
+
+/**
+ *
+ * @author Axelle
+ */
+public class AlgoRandom {
+    
+    public static void makeTourneesRandom()
+    {
+        boolean OK1 = true;
+        boolean OK2 = false;
+        List<CommandeClient> clientsTrains = new ArrayList<>();
+        List<CommandeClient> clientsCamions = new ArrayList<>();
+        
+        
+        // Dans un premier temps on a besoin de récupérer les instances de chaques objets
+        JpaVehiculeDao          daoVehicule         = JpaVehiculeDao.getInstance();
+        JpaVehiculeActionDao    daoVehiculeAction   = JpaVehiculeActionDao.getInstance();
+        JpaLieuDao              daoLieu             = JpaLieuDao.getInstance();
+        JpaDepotDao             daoDepot            = JpaDepotDao.getInstance();
+        JpaCommandeClientDao    daoCommandeClient   = JpaCommandeClientDao.getInstance();
+        JpaSolutionDao          daoSolution         = JpaSolutionDao.getInstance();
+        JpaSwapBodyDao          daoSwapBodyDao      = JpaSwapBodyDao.getInstance();
+        JpaTrajetDao            daoTrajet           = JpaTrajetDao.getInstance();
+        
+        List<Depot> depots = (List<Depot>) daoDepot.findAll();
+        Depot depot = depots.get(0);
+        clientsTrains = daoCommandeClient.findAllTrains();
+        clientsCamions = daoCommandeClient.findAllCamions();
+        
+        
+        for(CommandeClient client : clientsTrains)
+        {
+            OK1 = true;
+            OK2 = false;
+            
+            HypoTournee tournee = new HypoTournee();
+            tournee.addLieu(depot);
+            tournee.addLieu(client);
+            
+            Trajet trajet = daoTrajet.find(depot, client);
+            
+            tournee.setDuree(tournee.getDuree() + Long.parseLong(trajet.getDuree() + ""));
+            
+            while(OK1 == true)
+            {
+                //Randomisation = ajout d'un lieu
+                //Ajout de la durée
+            }
+            Lieu dernierLieu = tournee.getLieux().get(tournee.getLieux().size());
+            tournee.removeLieu(dernierLieu);
+            
+            tournee.addLieu(depot);
+                                
+            //while()
+                
+            
+            
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/java/ordo/algo/HypoTournee.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordo.algo;
+
+import ordo.cplex.CplexTournee;
+import ordo.data.Constantes;
+
+/**
+ *
+ * @author Axelle
+ */
+public class HypoTournee extends CplexTournee {
+    private long duree;
+    private float quantite;
+
+    public long getDuree() {
+        return duree;
+    }
+
+    public void setDuree(long duree) {
+        this.duree = duree;
+    }
+
+    public float getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(float quantite) {
+        this.quantite = quantite;
+    }
+    
+    public boolean isTooLong()
+    {
+        return (this.duree > Constantes.dureeMaxTournee);
+    }
+    
+    public boolean isTooFull()
+    {
+        return (this.quantite > Constantes.capaciteMax);
+    }
+}
